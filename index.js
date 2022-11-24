286c5b3bb075abb264146d36277fdcd4d8677b64
vedant-204 <katyayana2003@gmail.com>
2022-11-24 21:32:21 +0530

    index.js

--- index.js
+++ index.js
@@ -13,13 +13,22 @@ app.intent('Show sample', (conv) => {
     conv.ask(new SimpleResponse ({
         speech: 'Here you go',
         text: 'Here your go',
-    })); 
-});
+    }));
+    conv.ask(new BasicCard({
+        title: 'Anya',
+        image: new Image({
+            url: 'https://imgs.search.brave.com/sX_9BVN9mz5_zR_YtiQwWNuX97MxiZAe1hZVV_HdfGo/rs:fit:144:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5Q/Yy1neno0eXAxM05f/dkRrMU41Mkh3QUFB/QSZwaWQ9QXBp',
+            alt: 'anya',
+        })
+    }));
+})
 
 eApp.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World')
 });
 
 eApp.post('/webhook', app);
 
-eApp.listen(3000)
+eApp.listen(3000, () => {
+    console.log('listening on https://localhost:3000')
+})
